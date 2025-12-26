3505e3d595f2f67a8fd85c3549a608a9d8d090e1
jcagroenen <j.c.a.groenen@gmail.com>
2025-12-26 09:45:00 +0100

    changed deploy.php

--- deploy.php
+++ deploy.php
@@ -7,16 +7,51 @@ require 'recipe/wordpress.php';
 
 set('repository', 'git@github.com:jcagroenen/droogijs.git');
 
-add('shared_files', []);
+add('shared_files', [
+    '.env'
+]);
 add('shared_dirs', []);
-add('writable_dirs', []);
+
+set('shared_dirs', [
+    'web/app/uploads',
+    'web/app/uploads-webpc'
+]);
+
+set('writable_dirs', [
+    'web/app/uploads',
+    'web/app/uploads-webpc'
+]);
 
 // Hosts
 
-host('')
-    ->set('remote_user', 'deployer')
-    ->set('deploy_path', '~/droogijs');
+host('droogijs.groenen-webdev.nl')
+    ->set('remote_user', 'juulst')
+    ->set('deploy_path', '/home/juulst/droogijs.groenen-webdev.nl')
+    ->set('branch', 'main');
+
+
+//Releases
+set('keep_releases', 3);
 
 // Hooks
 
 after('deploy:failed', 'deploy:unlock');
+
+
+task('deploy:theme:composer-install', function () {
+    run('cd {{release_path}}/web/app/themes/droogijs && composer install');
+});
+
+task('deploy:theme:build', function () {
+    run('cd {{release_path}}/web/app/themes/droogijs && npm ci && npm run build');
+});
+
+
+desc('Deploys project');
+task('deploy', [
+    'deploy:prepare',
+    'deploy:vendors',
+    'deploy:theme:composer-install',
+    'deploy:theme:build',
+    'deploy:publish',
+]);
